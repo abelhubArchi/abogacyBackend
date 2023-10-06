934e21664a9073c1a9d8a653a1ab5507483c3385
abelhubArchi <bolevel12@gmail.com>
2023-10-06 00:09:25 -0400

    nuevo dominio ania

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ app.use(multer({
 const cors = require('cors');
 const { log } = require('console');
 var corsOptions = {
-  origin: "https://abogacy-519e5409953a.herokuapp.com",
+  origin: ["http://192.168.0.18:5173", "https://abogacy-519e5409953a.herokuapp.com", "abogacyio.netlify.app"],
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 app.use(cors(corsOptions));
