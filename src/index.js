a02cf3400ca7f811b1853ba4edd99662ca7e8849
abelhubArchi <bolevel12@gmail.com>
2023-10-10 23:21:34 -0400

    esta

--- src/index.js
+++ src/index.js
@@ -36,7 +36,16 @@ app.use(cors(corsOptions));
 // settings
 app.set('port', process.env.PORT || 3000);
 log(process.env.PORT || 3000)
+
 // routes
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://abogacyio.netlify.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 app.use(require('./routes/index.js'));
 app.use(require('./routes/chat.js'));
 app.use(require('./routes/casos.js'))
